2ee5cb34f8fb33093dd0817e5e0953007fe30a16
Renan Lopes <renanlopescoder@gmail.com>
2017-01-10 20:09:55 -0300

    Teste Servidor

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-app.set('io',io);
-var porta = process.env.PORT || 3000;
+var http = require('http');
 var app = require('./config/express');
 require('./config/database');
 
-http.listen(porta, function(){
-	 console.log("servidor rodando no heroku");
+var port =  process.env.port || 3000;
+
+app.listen(port, function(){
+	console.log('Servidor Iniciado');
 }); 
 
