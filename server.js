23e6156cc39558df259e90c4c10b270694be25f4
Renan Lopes <renanlopescoder@gmail.com>
2017-01-10 16:12:16 -0300

    Database changes and server port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var app = require('./config/express');
 require('./config/database');
 
-http.createServer(app).listen(8080, function(){
+http.createServer(app).listen(3000, function(){
 	console.log('Servidor Iniciado');
 }); 
 
