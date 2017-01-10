a3a8a5edb6403cbb92fb6bb25596c15bf4272d45
Renan Lopes <renanlopescoder@gmail.com>
2017-01-10 17:39:13 -0300

    Some corrections for Heroku server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var app = require('./config/express');
 require('./config/database');
 
-http.createServer(app).listen(3000, function(){
+http.createServer(app).listen(5000, function(){
 	console.log('Servidor Iniciado');
 }); 
 
