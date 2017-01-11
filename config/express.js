ce8a9d1ce85a0b5b47f6bb31451527d3b285d19b
Renan Lopes <renanlopescoder@gmail.com>
2017-01-11 08:14:11 -0300

    Bugfix incompatibility with Consign and Heroku server
    - Solving the incompatibility removing the consign and applying the express-load

--- config/express.js
+++ config/express.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
-var consign = require('consign');
+var load = require('express-load');
 
 //Configurações do Express
 
@@ -10,8 +10,7 @@ var consign = require('consign');
 	
 	app.use(express.static('./public')); // aplicando
 
-	consign()
-	.include('app/models') // Irá carregar os modelos como no hibernate
+	load('app/models') // Irá carregar os modelos como no hibernate
 	.then('app/api')
 	.then('app/routes')
 	.into(app);
